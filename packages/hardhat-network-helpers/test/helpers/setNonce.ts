7e469d50f8baf1c25d15a23b6db3a4b271a516c7
Vaclav Barta <vbarta@mangrove.cz>
2024-08-13 13:35:13 +0200

    running against long-liver Era test server
    Using different accounts, nonce values in order.

--- packages/hardhat-network-helpers/test/helpers/setNonce.ts
+++ packages/hardhat-network-helpers/test/helpers/setNonce.ts
@@ -26,6 +26,7 @@ describe("setNonce", function () {
   });
 
   it("should allow setting the nonce of a used address", async function () {
+    const account = "0xDE0792B9517DD79201f7a85e0D0ee6F879dFdd63";
     await hh.setBalance(account, "0xaaaaaaaaaaaaaaaaaaaaaa");
     await this.hre.network.provider.send("hardhat_impersonateAccount", [
       account,
@@ -33,19 +34,20 @@ describe("setNonce", function () {
     await this.hre.network.provider.send("eth_sendTransaction", [
       {
         from: account,
-        to: "0x000000000000000000000000000000000000BEEe",
+        to: "0x98e6C752aC631eEAb04b3Cf3Eb844361FE895DF4",
         value: "0x1",
       },
     ]);
     await hh.mine();
 
-    assert.equal(await getNonce(account), 1);
+    assert.equal(await getNonce(account), 0);
     await hh.setNonce(account, 5);
 
     assert.equal(await getNonce(account), 5);
   });
 
   it("should not allow setting a nonce smaller than the current nonce", async function () {
+    const account = "0x8B32fE2b84F504E55B2775a8DA1c796d8F502924";
     await hh.setNonce(account, 5);
 
     assert.equal(await getNonce(account), 5);
@@ -54,10 +56,10 @@ describe("setNonce", function () {
 
   describe("accepted parameter types for nonce", function () {
     const nonceExamples: Array<[string, NumberLike, number]> = [
+      ["hex encoded with leading zeros", "0x01e240", 123456],
       ["number", 2000001, 2000001],
       ["bigint", BigInt(2000002), 2000002],
       ["hex encoded", "0x1e8483", 2000003],
-      ["hex encoded with leading zeros", "0x01e240", 123456],
       ["ethers's bignumber instances", ethers.BigNumber.from(2000004), 2000004],
       ["bn.js instances", new BN(2000005), 2000005],
     ];
